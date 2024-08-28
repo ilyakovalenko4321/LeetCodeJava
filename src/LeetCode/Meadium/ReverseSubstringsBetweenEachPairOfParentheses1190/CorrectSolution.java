a89520a37e2045d11671ea65d8c6462305daac26
1lya_kovalenko <kovalenkoilyaden@gmail.com>
2024-08-28 13:29:33 +0300

    Commit After Adding 1190 problem

--- src/LeetCode/Meadium/ReverseSubstringsBetweenEachPairOfParentheses1190/CorrectSolution.java
+++ src/LeetCode/Meadium/ReverseSubstringsBetweenEachPairOfParentheses1190/CorrectSolution.java
@@ -1,6 +1,48 @@
 package LeetCode.Meadium.ReverseSubstringsBetweenEachPairOfParentheses1190;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 public class CorrectSolution {
+
+    static int[] index;
+    static char[] chars;
+    static int N;
+    public static String reverseParentheses(String s) {
+        index = new int[1];
+        chars = s.toCharArray();
+        N = chars.length;
+
+        return helper(new StringBuilder());
+    }
+
+    static String helper(StringBuilder builder){
+        if(index[0] == N){
+            return builder.toString();
+        }
+
+        while(index[0] < N){
+            char ch = chars[index[0]];
+            if(ch == '('){
+                index[0]++;
+                String temp = helper(new StringBuilder());
+                builder.append(temp);
+            } else if(ch == ')'){
+                return builder.reverse().toString();
+            } else {
+                builder.append(ch);
+            }
+            index[0]++;
+        }
+
+        return builder.toString();
+    }
+
     public static void main(String[] args) {
+        System.out.println(reverseParentheses("(ed(et(oc))el)"));
     }
 }
+
+
--- src/LeetCode/Meadium/ReverseSubstringsBetweenEachPairOfParentheses1190/Solution.java
+++ src/LeetCode/Meadium/ReverseSubstringsBetweenEachPairOfParentheses1190/Solution.java
@@ -1,6 +1,29 @@
 package LeetCode.Meadium.ReverseSubstringsBetweenEachPairOfParentheses1190;
 
+import java.util.*;
+
 public class Solution {
+
+    public static String reverseParentheses(String s) {
+
+        Deque<Integer> indStack = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+
+        for(char character: s.toCharArray()){
+            if (character == '(') {
+                indStack.push(res.length());
+            } else if (character==')') {
+                int startInd = indStack.pop();
+                String reversed = new StringBuilder(res.substring(startInd)).reverse().toString();
+                res.replace(startInd, res.length(), reversed);
+            }else{
+                res.append(character);
+            }
+        }
+       return res.toString();
+    }
+
     public static void main(String[] args) {
+        System.out.println(reverseParentheses("(ed(et(oc))el)"));
     }
 }
